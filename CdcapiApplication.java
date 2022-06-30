0479042eba212b9b0e4f3b58688f588fb86e00a2
William Ford <96089722+wmaf@users.noreply.github.com>
2022-06-30 15:26:19 -0400

    Add files via upload

---
+++ CdcapiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.cdcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcapiApplication.class, args);
+	}
+
+}
